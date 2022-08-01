60815b4b3660b53831aa5f750aef87658fed542c
AhmadAlharbi <ahmaadzaid7@gmail.com>
2022-08-01 23:10:18 +0300

    fix layouts and add js feature

--- js/script.js
+++ js/script.js
@@ -1,46 +1,39 @@
-// const btn = document.getElementById('menu-btn')
-// const menu = document.getElementById('menu')
+const url_input = document.querySelector("body > div > input");
+const submitButton = document.getElementById("submit");
+// const api_base = "https://api.shrtco.de/v2/shorten?url=kooora.com";
+const api_base = "https://api.shrtco.de/v2/shorten?url=";
+const linksDiv = document.getElementById("links");
+const innerLinksDiv = document.getElementById("inner-links");
+//eventlistner
+submitButton.addEventListener("click", (e) => {
+  getUrl(api_base + url_input.value);
+});
 
-// const input = document.getElementById('link-input')
-// const linkForm = document.getElementById('link-form')
-// const errMsg = document.getElementById('err-msg')
-
-// btn.addEventListener('click', navToggle)
-// linkForm.addEventListener('submit', formSubmit)
-
-// // Toggle Mobile Menu
-// function navToggle() {
-//   btn.classList.toggle('open')
-//   menu.classList.toggle('flex')
-//   menu.classList.toggle('hidden')
-// }
-
-// // Validate a URL
-// function validURL(str) {
-//   var pattern = new RegExp(
-//     '^(https?:\\/\\/)?' + // protocol
-//       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-//       '((\\d{1,3}\\.){3}\\d{1,3}))' +
-//       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-//       '(\\?[;&a-z\\d%_.~+=-]*)?' +
-//       '(\\#[-a-z\\d_]*)?$',
-//     'i'
-//   )
-//   return !!pattern.test(str)
-// }
-
-// function formSubmit(e) {
-//   e.preventDefault()
-
-//   if (input.value === '') {
-//     errMsg.innerHTML = 'Please enter something'
-//     input.classList.add('border-red')
-//   } else if (!validURL(input.value)) {
-//     errMsg.innerHTML = 'Please enter a valid URL'
-//     input.classList.add('border-red')
-//   } else {
-//     errMsg.innerHTML = ''
-//     input.classList.remove('border-red')
-//     alert('Success')
-//   }
-// }
+const generateDiv = (url, short) => {
+  const html = `<div
+    class="link flex justify-between items-center bg-white py-4 px-6 rounded-sm"
+  >
+    <h4>${url}</h4>
+    <div class="flex items-center space-x-4">
+      <a href="${url}" class="text-cyan text-2xl">${short} </a>
+      <button
+        class="bg-cyan text-white py-2 px-6 rounded-md hover:opacity-80"
+      >
+        Copy
+      </button>
+    </div>
+  </div>`;
+  linksDiv.innerHTML += html;
+};
+// generateDiv("www.google.com", "go");
+// generateDiv("www.google.com", "go");
+const getUrl = async (url) => {
+  fetch(url);
+  // .then((response) => response.json())
+  // .then((data) => console.log(data.result.original_link));
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.result.original_link);
+  generateDiv(data.result.original_link, data.result.short_link);
+};
+// getUrl("https://api.shrtco.de/v2/shorten?url=kooora.com");
